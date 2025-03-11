bce99fe6812ca7f6a1a3f3ccb5fb34b61528946d
798118 <164620581+TURBOTHAI@users.noreply.github.com>
2025-03-11 12:45:02 +0100

    radix sortering

--- algoritmer/radixsort/Rardixsortering.java
+++ algoritmer/radixsort/Rardixsortering.java
@@ -3,14 +3,14 @@ package radixsort;
 public class Rardixsortering {
 	
 	
-	public static void main(String[]args) {
+	public static void main(String[]args) {//lager array med tall i ikkje riktig rekke følge
 		int[] array= {8,2,5,3,9,4,7,6,5};
 		
 		radixSort(array);
 		
 		 for (int i : array) {
 			System.out.print(i+" ");
-		}
+		}// printer listen ut
 		
 	}
 	
@@ -21,37 +21,38 @@ public class Rardixsortering {
 				maks=i;
 			}
 		}
-		return maks;
+		return maks;//finner maks verdien i listen
 	}
 	
 	private static void tellerSort(int[] array, int exp) {
-		int n = array.length;
-		int[] teller=new int[10]; //teller array for sifre (0,9)	
-		
-		for(int i:array) {
-			teller[(i/exp)%10]++;
-		}
-		
-		for(int i=1;i<10;i++) {
-			teller[i]+=teller[i-1];
-			
-		}
-		
-		
-		for(int i =n-1;i>=0;i--) {
-			int siffer=(array[i]/exp%10);
-			while(teller[siffer]-1!=i) {
-				int korrektindeks=teller[siffer]-1;				
-				int temp=array[korrektindeks];
-				array[korrektindeks]=array[i];
-				array[i]=temp;
-				teller[siffer]--;
-				siffer=(array[i]/exp)%10;
-			}
-			
-			teller[siffer]--;
-		}
+	    int n = array.length;
+	    int[] output = new int[n]; // Hjelpearray for riktig rekkefølge
+	    int[] teller = new int[10]; // Teller for sifre (0-9)
+
+	    // Fyll teller-arrayet (hvor mange ganger hvert siffer forekommer)
+	    for (int i = 0; i < n; i++) {
+	        int siffer = (array[i] / exp) % 10;
+	        teller[siffer]++;
+	    }
+
+	    // Endre teller slik at den viser posisjon i output-arrayet
+	    for (int i = 1; i < 10; i++) {
+	        teller[i] += teller[i - 1];
+	    }
+
+	    // Bygg output-arrayet ved å plassere elementene i riktig rekkefølge
+	    for (int i = n - 1; i >= 0; i--) {
+	        int siffer = (array[i] / exp) % 10;
+	        output[teller[siffer] - 1] = array[i]; // Plasser riktig i output
+	        teller[siffer]--; // Flytt en posisjon bakover
+	    }
+
+	    // Kopier sorterte tall tilbake til originalt array
+	    for (int i = 0; i < n; i++) {
+	        array[i] = output[i];
+	    }
 	}
+
 	
 	public static void radixSort(int[] array) {
 		
